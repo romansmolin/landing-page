b8bd8d23ba0e719f861a35cca605b10094046902
Roman Smolins <romasmolin29@gmail.com>
2023-12-05 10:20:48 +0200

    First step validation

--- client/src/js/modules/formData.js
+++ client/src/js/modules/formData.js
@@ -3,3 +3,7 @@ const formData = {};
 function updatePropertyType(propertyType) {
     formData["propertyType"] = propertyType;
 }
+
+function updateZipCode(zipCode, inputId) {
+    formData[inputId] = zipCode
+}
--- client/src/js/modules/validation.js
+++ client/src/js/modules/validation.js
@@ -8,7 +8,7 @@ function handleChoiceButtonClick(button, updateFunction) {
     updateFunction(data);
 }
 
-function validateZipCode(input, errorId) {
+function validateZipCode(input, errorId, updateFunction) {
     const validationRegex = /^[0-9]*$/i;
     const errorMessage = "Please enter a valid 5-digit zip code";
     const length = 5;
@@ -20,11 +20,42 @@ function validateZipCode(input, errorId) {
 
     if (!validationRegex.test(zipCode) || zipCode.length !== length) {
         errorElement.textContent = errorMessage;
+        updateFunction("", input.id);
     } else {
         errorElement.textContent = "";
+        updateFunction(zipCode, input.id);
     }
 }
 
 function validateFirstStep() {
-    console.log(formData)
+    if (!formData.dzip || !formData.ozip || !formData.propertyType) {
+        const filedsToValidate = [
+            {
+                name: 'propertyType',
+                errorSpan: document.getElementById('property-error'),
+                errorMessage: 'You Need To Choose One Option.'
+            },
+            {
+                name: 'ozip',
+                errorSpan: document.getElementById('ozip-error'),
+                errorMessage: 'Please enter Moving From Zip Code.'
+            },
+            {
+                name: 'dzip',
+                errorSpan: document.getElementById('dzip-error'),
+                errorMessage: 'Please enter Moving To Zip Code.'
+            },
+        ]
+
+        for (const { name, errorSpan, errorMessage } of filedsToValidate) {
+            if (!formData[name]) {
+                errorSpan.textContent = errorMessage;
+            } else {
+                errorSpan.textContent = "";
+            }
+        }
+    } else {
+        navigateStep(2, 1)
+    }
 }
+
